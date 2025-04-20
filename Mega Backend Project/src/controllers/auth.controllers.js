180559217bd1dae9a4860459e010871a57d6819b
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-20 18:51:59 +0530

    5 April - Backend - 3

--- Mega Backend Project/src/controllers/auth.controllers.js
+++ Mega Backend Project/src/controllers/auth.controllers.js
@@ -3,7 +3,70 @@ const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password, role } = req.body;
 
   //validation
-  registrationValidation(body);
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const resendEmailVerification = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+const resetForgottenPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+export {
+  changeCurrentPassword,
+  forgotPasswordRequest,
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  resendEmailVerification,
+  resetForgottenPassword,
+  verifyEmail,
+};
